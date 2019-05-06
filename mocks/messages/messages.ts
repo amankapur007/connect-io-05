8adcc8e49ba08915b145395b53a0670a0aedba93
amankapur007 <akapur07@gmail.com>
2019-05-06 21:56:08 +0530

    4 files modified

--- mocks/messages/messages.ts
+++ mocks/messages/messages.ts
@@ -5,7 +5,7 @@ const userList=USER_LIST;
 const messageList:Message[]=[];
 
 for(var i=0;i<userList.length;i++){
-  messageList.push({user:userList[i],date:new Date()});
+  messageList.push({user:userList[i],date:new Date(),lastMessage:"Hello "+userList[i].firstName});
 }
 
 export const MESSAGE_LIST=messageList;
--- mocks/users/users.ts
+++ mocks/users/users.ts
@@ -7,7 +7,7 @@ const userList:User[]=[{
 },{
 firstName:'Aman 1',
   lastName:'Kapur 1',
-  avatar:'',
+  avatar:'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y',
   email:'akapur07@gmail.com'
 },{
 firstName:'Aman 2',
