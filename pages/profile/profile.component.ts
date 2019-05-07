0ff568e4e3c03201bdc2837b5b2f62b115c8c06e
amankapur007 <akapur07@gmail.com>
2019-05-07 19:44:49 +0530

    4 files modified

--- pages/profile/profile.component.ts
+++ pages/profile/profile.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Platform, Navbar, NavController, NavParams,IonicPage, ToastController } from 'ionic-angular';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+import { Account } from '../../models/account/account.model';
 
+@IonicPage({})
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -7,9 +12,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+constructor(private navCtrl:NavController, private navParam:NavParams, private afAuth:AngularFireAuth, private toast:ToastController) { }
 
   ngOnInit() {
   }
 
+  ionViewDidLoad(){
+    const result = this.navParam.get("result");
+    console.log(result);
+  }
 }
--- pages/register/register.component.ts
+++ pages/register/register.component.ts
@@ -3,6 +3,7 @@ import { Platform, Navbar, NavController, NavParams,IonicPage, ToastController }
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
 import { Account } from '../../models/account/account.model';
+import { ProfileComponent } from '../profile/profile.component';
 
 
 @IonicPage({})
@@ -14,18 +15,21 @@ import { Account } from '../../models/account/account.model';
 export class RegisterComponent implements OnInit {
 
   account = {} as Account;
-  constructor(private afAuth:AngularFireAuth, private toast:ToastController) { }
+  constructor(private navCtrl:NavController, private navParam:NavParams, private afAuth:AngularFireAuth, private toast:ToastController) { }
 
   ngOnInit() {
   }
 
   async register(){
     try{
-    const result = await this.afAuth.auth.createUserWithEmailAndPassword(this.account.email, this.account.password);
+    //const result = await this.afAuth.auth.createUserWithEmailAndPassword(this.account.email, this.account.password);
     this.toast.create({
       message:"User is successfully registered",
       duration:3000
     }).present();
+    this.navCtrl.push(ProfileComponent,{
+      "result":null
+    });
   }catch(e){
     console.log(e);
     this.toast.create({
