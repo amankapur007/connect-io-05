202f7831c4ff391dd3139e264491407d6679651f
amankapur007 <akapur07@gmail.com>
2019-05-08 00:14:00 +0530

    3 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { LoginComponent } from '../pages/login/login.component';
 
@@ -11,12 +9,10 @@ import { LoginComponent } from '../pages/login/login.component';
 export class MyApp {
   rootPage:any = LoginComponent;
 
-  constructor(platform:Platform, splashScreen:SplashScreen, statusBar: StatusBar){
+  constructor(platform:Platform){
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
     });
   }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
