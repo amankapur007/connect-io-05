10438a7aa7f757ba89296a0e69276be80ea79e51
amankapur007 <akapur07@gmail.com>
2019-05-08 00:26:28 +0530

    3 files modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
@@ -31,6 +33,7 @@ import { TabsComponent } from '../pages/tabs/tabs.component';
   ],
   providers: [
     {provide: ErrorHandler, useClass: IonicErrorHandler},
+    SplashScreen,StatusBar
   ]
 })
 export class AppModule {}
